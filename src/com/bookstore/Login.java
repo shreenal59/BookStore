691210955553706d7b4627f214a41ed39e09d36e
shreenal59 <sdp68959@uga.edu>
2020-12-03 16:42:12 -0500

    update checkout

--- src/com/bookstore/Login.java
+++ src/com/bookstore/Login.java
@@ -23,6 +23,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
         
         String uname = request.getParameter("uname");
--- src/com/bookstore/checkout1.java
+++ src/com/bookstore/checkout1.java
@@ -1,11 +1,15 @@
 package com.bookstore;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -43,69 +47,91 @@ public class checkout1 extends HttpServlet {
 		String cardNo = request.getParameter("cardNo");
 		String exDate = request.getParameter("Exp_date");
 		String cardType = request.getParameter("cardType");
-		
-		String promo = request.getParameter("promo");
-		
+		java.util.Date ex_Date = null;
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookstore","root","4122");
-			String query1 = "";
-			String query2 = "";
-			String query3 = "";
-			if (billing.equals("true")) {
-				query1 = "UPDATE bookstore.address SET street = '" + streetB + "', city = '" + cityB +"', state = '"
-						+ stateB +"', zip = '"+ zipB +"' Where customer_id = '"+cusId+ "' AND address_type = 'B';";
-			} else {
-				query1 = "insert into Address ( customer_id, street, city, state, zip, address_type) "
-						+ "	values ( '"+ cusId +"', '"+streetB +"', '"+cityB+"', '"+stateB+"', '"+zipB+"', 'B');";
-			}
+			ex_Date = new SimpleDateFormat("yyyy-MM-dd").parse(exDate);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
+		java.sql.Date sqlExDate = new Date(ex_Date.getTime());
+		long millis=System.currentTimeMillis();
+		Date today = new java.sql.Date(millis);
+		PrintWriter out = response.getWriter();
+		boolean flag = true;
+		if(today.compareTo(sqlExDate) > 0) {
+			out.println("<script>");
+            out.println("alert('Card is Exipired, change to another Card');");
+            out.println("</script>");
+            flag = false;
+            RequestDispatcher rd = request.getRequestDispatcher("checkout1.jsp");
+			rd.include(request, response);
 			
-			if (shipping.equals("true")) {
-				query2 = "UPDATE bookstore.address SET street = '" + streetS + "', city = '" + cityS +"', state = '"
-						+ stateS +"', zip = '"+ zipS +"' Where customer_id = '"+cusId+ "' AND address_type = 'S';";
-			} else {
-				query2 = "insert into Address ( customer_id, street, city, state, zip, address_type) "
-						+ "	values ( '"+ cusId +"', '"+streetS +"', '"+cityS+"', '"+stateS+"', '"+zipS+"', 'S');";
-			}
+		}
+		
+		if (flag) {
+			String promo = request.getParameter("promo");
 			
-			if (card.equals("true")) {
-				query3 = "UPDATE bookstore.payment_card SET card_number = aes_encrypt('"+cardNo+"', '4050'), "
-						+ "expiration_date = '"+exDate+"', card_type = '"+cardType+"' Where customer_id = '"+cusId+"';";
-			} else {
-				String query4 = "Select max(card_id) FROM bookstore.payment_card;";
-				Statement st4 = con.createStatement();
-				ResultSet rs4 =st4.executeQuery(query4);
-				int cardId = -1;
-				while(rs4.next()) {
-					cardId = rs4.getInt(1) + 1; 
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookstore","root","4122");
+				String query1 = "";
+				String query2 = "";
+				String query3 = "";
+				if (billing.equals("true")) {
+					query1 = "UPDATE bookstore.address SET street = '" + streetB + "', city = '" + cityB +"', state = '"
+							+ stateB +"', zip = '"+ zipB +"' Where customer_id = '"+cusId+ "' AND address_type = 'B';";
+				} else {
+					query1 = "insert into Address ( customer_id, street, city, state, zip, address_type) "
+							+ "	values ( '"+ cusId +"', '"+streetB +"', '"+cityB+"', '"+stateB+"', '"+zipB+"', 'B');";
 				}
-				query3 = "insert into Payment_Card ( card_id, customer_id, card_number, expiration_date, card_type)"
-						+ "	values( '"+cardId+"', '"+cusId+"', aes_encrypt('"+cardNo+"', '4050'), '"+exDate+"', '"+cardType+"');";
 				
+				if (shipping.equals("true")) {
+					query2 = "UPDATE bookstore.address SET street = '" + streetS + "', city = '" + cityS +"', state = '"
+							+ stateS +"', zip = '"+ zipS +"' Where customer_id = '"+cusId+ "' AND address_type = 'S';";
+				} else {
+					query2 = "insert into Address ( customer_id, street, city, state, zip, address_type) "
+							+ "	values ( '"+ cusId +"', '"+streetS +"', '"+cityS+"', '"+stateS+"', '"+zipS+"', 'S');";
+				}
+				
+				if (card.equals("true")) {
+					query3 = "UPDATE bookstore.payment_card SET card_number = aes_encrypt('"+cardNo+"', '4050'), "
+							+ "expiration_date = '"+exDate+"', card_type = '"+cardType+"' Where customer_id = '"+cusId+"';";
+				} else {
+					String query4 = "Select max(card_id) FROM bookstore.payment_card;";
+					Statement st4 = con.createStatement();
+					ResultSet rs4 =st4.executeQuery(query4);
+					int cardId = -1;
+					while(rs4.next()) {
+						cardId = rs4.getInt(1) + 1; 
+					}
+					query3 = "insert into Payment_Card ( card_id, customer_id, card_number, expiration_date, card_type)"
+							+ "	values( '"+cardId+"', '"+cusId+"', aes_encrypt('"+cardNo+"', '4050'), '"+exDate+"', '"+cardType+"');";
+					
+				}
+				
+				
+				Statement st1 = con.createStatement();
+				st1.executeUpdate(query1);
+				
+				Statement st2 = con.createStatement();
+				st2.executeUpdate(query2);
+				
+				Statement st3 = con.createStatement();
+				st3.executeUpdate(query3);
+				
+				//Cookie cookie = new Cookie("promo", promo);
+				//response.addCookie(cookie);
+				HttpSession session = request.getSession();
+				session.setAttribute("promo", promo);
+				
+				RequestDispatcher rd = request.getRequestDispatcher("checkout2.jsp");
+	            rd.include(request, response);
+	
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
-			
-			Statement st1 = con.createStatement();
-			st1.executeUpdate(query1);
-			
-			Statement st2 = con.createStatement();
-			st2.executeUpdate(query2);
-			
-			Statement st3 = con.createStatement();
-			st3.executeUpdate(query3);
-			
-			//Cookie cookie = new Cookie("promo", promo);
-			//response.addCookie(cookie);
-			HttpSession session = request.getSession();
-			session.setAttribute("promo", promo);
-			
-			RequestDispatcher rd = request.getRequestDispatcher("checkout2.jsp");
-            rd.include(request, response);
-
-			
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 		
 	}
